e8d048f2d14e64a1345acd64225206421a584a8e
BHASVIC-JoshFrancis22 <137882415+BHASVIC-JoshFrancis22@users.noreply.github.com>
2023-09-26 10:43:18 +0100

    Add files via upload

---
+++ Francis Casino/Francis Casino/Francis_CasinoApp.swift
@@ -0,0 +1,17 @@
+//
+//  Francis_CasinoApp.swift
+//  Francis Casino
+//
+//  Created by Josh Francis on 26/09/2023.
+//
+
+import SwiftUI
+
+@main
+struct Francis_CasinoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
